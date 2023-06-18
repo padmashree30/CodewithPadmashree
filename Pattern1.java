651f8ea61e22ea48f3f09579f1e0318c4646b275
Padmaashree <nshree1255@gmail.com>
2023-06-18 19:07:33 +0530

    new

--- Pattern1.java
+++ Pattern1.java
@@ -6,7 +6,7 @@ public class Pattern1 {
 		int i, j;
 		for(i=1;i<=6;i++) {
 			for(j=1;j<=i;j++) {
-				System.out.print(" * ");
+				System.out.print(" o ");
 			}
 			System.out.println("\n");
 		}
