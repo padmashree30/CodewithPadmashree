8ddad054b3d3595e6e265a6e21a3c6065f967812
Padmaashree <nshree1255@gmail.com>
2023-06-19 09:48:54 +0530

    remove file

---
+++ Remove.java
@@ -0,0 +1,15 @@
+package LogicPrograms;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		int i, j;
+		for(i=1;i<=16;i++) {
+			for(j=1;j<=i;j++) {
+				System.out.print(" o ");
+				System.out.print(" Padmas ");
+			}
+			System.out.println("\n \n");
+		}
+	}
+}
